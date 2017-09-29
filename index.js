585a5e95217cd61dede5001f66eca4ae7829e75e
tiven.wang <i.tiven.wang@gmail.com>
2017-09-29 18:07:00 +0800

    testing async

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 'use strict';
 
-var User = function() {
+var User = function(username) {
+  this.username = username;
 };
 
 User.prototype.save = function (callback) {
+  let that = this;
   setTimeout(function() {
-    callback('error')
+    that.status = "saved";
+    callback()
   }, 1000);
 };
+
+User.prototype.update = function (username) {
+  this.username = username;
+  let that = this;
+  return new Promise((resolve, reject) => {
+    setTimeout(function() {
+      that.status = "updated";
+      if(username) {
+        resolve(that);
+      }else {
+        reject("update fail!");
+      }
+    }, 1000);
+  });
+};
+
+User.prototype.delete = async function (username) {
+  await deleteUser(this);
+};
+
+function deleteUser(user) {
+  return new Promise((resolve, reject) => {
+    setTimeout(()=> {
+      resolve();
+    });
+  });
+}
 
 module.exports = User;
--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 var assert = require('assert');
 var User = require('../index');
 
+// Synchronous work/test
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
@@ -9,14 +10,54 @@ describe('Array', function() {
   });
 });
 
+// Asynchronous work/test
 describe('User', function() {
   describe('#save()', function() {
     it('should save without error', function(done) {
-      var user = new User('Luna');
+      var user = new User('Tiven');
       user.save(function(err) {
-        if (err) done(err);
-        else done();
+        if (err) {
+          done(err);
+        }else {
+          assert.equal(user.status, "saved", "User should be saved");
+          done();
+        }
       });
     });
+  });
+});
+
+// Promise async work/test
+describe('User', function() {
+  describe('#update()', function() {
+    let user;
+    before(function() {
+      user = new User('Tiven');
+    });
+
+    it('should update without error', function() {
+      return user.update('tiwen').then((user)=> {
+        assert.equal(user.status, "updated", "User should be updated");
+      });
+    });
+
+    it('should update with error', function() {
+      return user.update();
+    });
+  });
+});
+
+// Async/Await async work/test
+describe('User', function() {
+  describe('#update()', function() {
+    let user;
+    before(function() {
+      user = new User('Tiven');
+    });
+
+    it('should delete without error', function() {
+      return user.delete();
+    });
+
   });
 });
